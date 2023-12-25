34722d1143b08310ed38c70eb9e5aacbc44d0d3f
Thien Nguyen <ngocthiennguyen2399@gmail.com>
2023-12-25 21:15:50 +0100

    update

---
+++ Assignment 2_Tom.R
@@ -0,0 +1,58 @@
+library(stats)
+library(MASS)
+library(mclust)
+library(HDclassif)
+
+# loading the data and getting the true values 
+load('mnist_task2.Rdata')
+true_labels <- as.numeric(as.factor(target))
+
+
+#Hierarchical clustering on squared Euclidean distances using the method of Ward
+dist_data <- dist(data, method = "euclidean",diag = TRUE, upper = TRUE)
+hier_clust <- hclust(dist_data,"ward.D2")
+
+#Cutting the tree to 4 clusters
+clust_hier_4 <- cutree(hier_clust, k = 4)
+table(clust_hier_4)
+
+#K-means clustering 
+kmeans_result <-kmeans(data,4,nstart=50,iter.max=200)
+varciance_k<-k4_data$betweenss/k4_data$totss
+
+#HDDC clustering AkjBkQkD with hierarchical clustering 
+hddc_AkjBkQkD_hier <- hddc(data, K = 4, model = "AkjBkQkD",d_select = "Cattell" ,init.vector = clust_hier_4, 
+                           threshold = 0.05)
+
+#HDDC clustering AkjBkQkD with kmeans
+hddc_AkjBkQkD_means <- hddc(data, K = 4, model = "AkjBkQkD",d_select = "Cattell" ,init.vector = kmeans_result, 
+                            threshold = 0.05)
+
+#HDDC clustering AkjBQkD with hierarchical clustering 
+hddc_AkjBQkD_hier<-hddc(data, K = 4, model = "AkjBQkD",d_select = "Cattell" ,init.vector = clust_hier_4, 
+                        threshold = 0.05)
+
+#HDDC clustering AkjBkQkD with kmeans
+hddc_AkjBQkD_means<-hddc(data, K = 4, model = "AkjBQkD",d_select = "Cattell" ,init.vector = kmeans_result, 
+                         threshold = 0.05)
+
+#Calculating the ARI for each method
+ari_hier <- adjustedRandIndex(true_labels, clust_hier_4)
+ari_kmeans <- adjustedRandIndex(true_labels, kmeans_result$cluster)
+ari_hddc_AkjBkQkD_hier <- adjustedRandIndex(true_labels, hddc_AkjBkQkD_hier$class)
+ari_hddc_AkjBQkD_hier<- adjustedRandIndex(true_labels, hddc_AkjBQkD_hier$class)
+ari_hddc_AkjBkQkD_k<- adjustedRandIndex(true_labels, hddc_AkjBkQkD_means$class)
+ari_hddc_AkjBQkD_k<- adjustedRandIndex(true_labels, hddc_AkjBQkD_means$class)
+
+
+#Creating a dataframe with the ARI values 
+ari_values <- c(ari_hier, ari_kmeans, ari_hddc_AkjBkQkD_hier, ari_hddc_AkjBQkD_hier, ari_hddc_AkjBkQkD_k, ari_hddc_AkjBQkD_k)
+method_names <- c('Hierarchical', 'K-means', 'HDDC AkjBkQkD Hierarchical', 'HDDC AkjBQkD Hierarchical', 'HDDC AkjBkQkD K-means', 'HDDC AkjBQkD K-means')
+ari_df <- data.frame(Method = method_names, ARI = ari_values)
+print(ari_df)
+
+# Visualizing the higest ARI with 2 PCA on centerd data 
+prcomp<- prcomp(data, center = TRUE, scale. = FALSE)
+prcomp
+pc2_data <- prcomp$x[, 1:2]
+plot(pc2_data, col = clust_hier_4, main = "Hierarchical clustering", xlab = "PC1", ylab = "PC2")
---
+++ multivariate_task3_camille.R
@@ -0,0 +1,88 @@
+install.packages("plfm")
+library(plfm)
+help(anger)
+head(anger)
+
+#a.
+
+# anger data is a 3D array
+# Aggregate across situations (the second dimension is for situations)
+person_behavior_aggregated <- apply(anger$data, MARGIN =3, FUN = rowSums)
+
+# Check the dimensions of the aggregated matrix
+dim(person_behavior_aggregated)
+# We have 101 persons and 8 behaviors, the result is a 101 x 8 matrix
+
+# compute squared Euclidean distances
+help(dist)
+EuclideanDistance <- dist(person_behavior_aggregated, method = "euclidean", 
+                         diag = TRUE, upper = TRUE)
+
+SquaredEuclideanDistance <- EuclideanDistance^2
+#should i use square here????????
+
+#SquaredEuclideanDistance 
+
+# hierarchical clustering Ward bimodal data 
+# cluster on squared Euclidean distance 
+hiclust_ward<- hclust(SquaredEuclideanDistance, "ward.D2") 
+par(pty="s") 
+plot(hiclust_ward,hang=-1) 
+#Ward’s method fails to capture the difference in the true modality of the two samples. 
+
+#Save the cluster membership variable of the 2-cluster solution
+clusters <- cutree(hiclust_ward, k = 2)
+clusters
+
+
+#b.
+anger$freq1
+dim(anger$freq1)
+
+# Compute profile vectors for each cluster
+# we are looking for a 2*8 matrix where 2 unique clusters with 8 different behaviours with cells as frequency count
+# Combine the aggregated matrix and cluster assignments
+data_with_clusters <- data.frame(person_behavior_aggregated, cluster = clusters)
+profile_vectors <- aggregate(. ~ cluster, data = data_with_clusters, sum)
+profile_vectors <- profile_vectors[, -1]
+dim(profile_vectors)
+profile_vectors
+
+# Define the new column names
+new_column_names <- c(
+  "fly off the handle",
+  "quarrel",
+  "leave",
+  "avoid",
+  "pour out one's hart",
+  "tell one's story",
+  "make up",
+  "clear up the matter"
+)
+
+# Assign the new column names to 'profile_vectors'
+colnames(profile_vectors) <- new_column_names
+
+final_freq1 <- rbind(anger$freq1, profile_vectors)
+
+
+
+#c
+install.packages("ca")
+library(ca)
+
+#H0: bahabior and situations are statistically independent
+#if the Pearson-Chi square test indicates that Xand Y are statistically 
+#dependent, it is meaningful to use CA to further study the nature of the 
+#relation between Xand Y.
+
+chisq.test(final_freq1)
+#p-value is small enough to reject null
+ca.out<-ca(final_freq1)
+#slide 19 of ch10
+
+summary(ca.out)
+#slide 20
+
+plot(ca.out,mass=TRUE,contrib="absolute", 
+     map="colprincipal",arrows=c(TRUE, FALSE),xlim=c(-1.4,2))
